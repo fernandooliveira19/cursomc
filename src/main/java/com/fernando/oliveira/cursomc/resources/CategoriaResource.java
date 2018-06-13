dc2780da35dc97b10f7ec413d2a6dbf49b0910b8
= <=>
2018-06-12 23:50:37 -0300

    Classe de dominio Categoria; Metodo de listagem de categoria;

--- src/main/java/com/fernando/oliveira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fernando/oliveira/cursomc/resources/CategoriaResource.java
@@ -1,23 +1,28 @@
 package com.fernando.oliveira.cursomc.resources;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fernando.oliveira.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
-public class CategoriaResource implements Serializable{
+public class CategoriaResource{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
