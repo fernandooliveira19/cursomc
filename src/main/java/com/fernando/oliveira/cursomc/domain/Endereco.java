88b324ff7d7ea8a3b2e47a73d52e1a83550af4f4
= <=>
2018-06-26 22:23:39 -0300

    endereço
    Signed-off-by: = <=>

---
+++ src/main/java/com/fernando/oliveira/cursomc/domain/Endereco.java
@@ -0,0 +1,17 @@
+package com.fernando.oliveira.cursomc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Endereco implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Endereco() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
