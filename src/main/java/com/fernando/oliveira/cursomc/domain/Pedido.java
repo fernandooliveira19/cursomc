7e30f9c7fb1148fb28fb9900341b55ec3a44ee60
fernandooliveira19 <fer.a.oliveira19@gmail.com>
2018-08-23 19:56:18 -0300

    subtotal e total do pedido
    Signed-off-by: fernandooliveira19 <fer.a.oliveira19@gmail.com>

--- src/main/java/com/fernando/oliveira/cursomc/domain/Pedido.java
+++ src/main/java/com/fernando/oliveira/cursomc/domain/Pedido.java
@@ -58,6 +58,17 @@ public class Pedido implements Serializable {
 		this.enderecoEntrega = enderecoEntrega;
 		this.cliente = cliente;
 	}
+	
+	public double getValorTotal() {
+		
+		double soma = 0.0;
+		
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		
+		return soma;
+	}
 
 
 	public Integer getId() {
